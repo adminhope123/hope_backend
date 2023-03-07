671214a5d8cd3d925d344ad24286f5415723ae3d
Your Name <you@example.com>
2023-03-07 09:32:44 +0530

    some chnages

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -12,39 +12,38 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Contracts\Session\Session;
 use App\Helpers\Helper;
 
-
-
-
 class EmployeeController extends Controller
 {
+    // .................... Admin Login....................
 
     public function adminlogin(Request $Req)
     {
-        $APIadmin_login = DB::table('admin_logins')->where([['email', '=', $Req->email]])->get()->first();
-
+        $APIadmin_login = DB::table('admin_logins')
+            ->where([['email', '=', $Req->email]])
+            ->get()
+            ->first();
 
         if ($APIadmin_login) {
-
             if (Hash::check($Req->password, $APIadmin_login->password)) {
-
                 $admin_login['status'] = 1;
-                $admin_login['message'] = "Login Successfully....";
+                $admin_login['message'] = 'Login Successfully....';
                 return $admin_login;
             } else {
                 $admin_login['status'] = 0;
-                $admin_login['message'] = "Password Not Matched....";
+                $admin_login['message'] = 'Password Not Matched....';
                 return $admin_login;
             }
         } else {
             $admin_login['status'] = 0;
-            $admin_login['message'] = "Email Not Matched....";
+            $admin_login['message'] = 'Email Not Matched....';
             return $admin_login;
         }
-
     }
 
-    public function Uattendence(Request $req){
+    // ............................User attendence details............................
 
+    public function Uattendence(Request $req)
+    {
         $data = new Uattendence();
         $data->employeeId = $req->employeeId;
         $data->absent = $req->absent;
@@ -65,10 +64,10 @@ class EmployeeController extends Controller
     public function Uattendenceupdate($employeeEditIdData)
     {
         Uattendence::whereId($employeeEditIdData)->update();
-        return "Record Updated";
+        return 'Record Updated';
     }
 
-    public function Uattendenceupdatesave(Request $req,$employeeEditIdData)
+    public function Uattendenceupdatesave(Request $req, $employeeEditIdData)
     {
         $data = Uattendence::find($employeeEditIdData);
         $data->employeeId = $req->input('employeeId');
@@ -81,6 +80,8 @@ class EmployeeController extends Controller
 
         return $data;
     }
+
+    // ...............................User timer...............................
 
     public function usertimer(Request $req)
     {
@@ -101,7 +102,7 @@ class EmployeeController extends Controller
         $data->absent = $req->absent;
         $data->totalSeconds = $req->totalSeconds;
         $data->totalTimeWork = $req->totalTimeWork;
-        $data -> save();
+        $data->save();
         return $data;
     }
 
@@ -115,10 +116,10 @@ class EmployeeController extends Controller
     public function usertimerupdate($employeeEditIdData)
     {
         Utimer::whereId($employeeEditIdData)->update();
-        return "Record Updated";
+        return 'Record Updated';
     }
 
-    public function usertimerupdatesave(Request $req,$employeeEditIdData)
+    public function usertimerupdatesave(Request $req, $employeeEditIdData)
     {
         $data = Utimer::find($employeeEditIdData);
         $data->employeeId = $req->input('employeeId');
@@ -142,9 +143,11 @@ class EmployeeController extends Controller
         return $data;
     }
 
+    // .................................User Profile................................
+
     public function userprofile(Request $req)
     {
-        $data = new Uprofile;
+        $data = new Uprofile();
         $data->E_Id = $req->E_Id;
         $data->fullname = $req->fullname;
         $data->post = $req->post;
@@ -155,62 +158,64 @@ class EmployeeController extends Controller
         $data->countries = $req->countries;
         $data->state = $req->state;
         $data->city = $req->city;
-        $data -> save();
+        $data->save();
         return $data;
     }
 
-    public function userlogin(Request $Req){
+    // ................................User Login................................
 
-        $APIuser_login = DB::table('employees')->where([['email', '=', $Req->email]])->get()->first();
+    public function userlogin(Request $Req)
+    {
+        $APIuser_login = DB::table('employees')
+            ->where([['email', '=', $Req->email]])
+            ->get()
+            ->first();
 
         if ($APIuser_login) {
-
             if (Hash::check($Req->password, $APIuser_login->password)) {
-
                 $user_login['status'] = 1;
-                $user_login['message'] = "Login Successfully....";
+                $user_login['message'] = 'Login Successfully....';
                 return $user_login;
             } else {
                 $user_login['status'] = 0;
-                $user_login['message'] = "Password Not Matched....";
+                $user_login['message'] = 'Password Not Matched....';
                 return $user_login;
             }
         } else {
             $user_login['status'] = 0;
-            $user_login['message'] = "Email Not Matched....";
+            $user_login['message'] = 'Email Not Matched....';
             return $user_login;
         }
-
     }
+
+    // ....................Employee Details....................
 
     public function employee(Request $req)
     {
-        $E_data = Employee::orderBy('id','desc')->first();
-        $prefix  = 'E';
-        $code_E = substr($E_data->E_Id,strlen($prefix)+1);
-        $E_last_number = ($code_E/1)*1;
-        $increment_last_number = $E_last_number+1;
+        $E_data = Employee::orderBy('id', 'desc')->first();
+        $prefix = 'E';
+        $code_E = substr($E_data->E_Id, strlen($prefix) + 1);
+        $E_last_number = ($code_E / 1) * 1;
+        $increment_last_number = $E_last_number + 1;
         $last_number_length = strlen($increment_last_number);
         $E_length = 3 - $last_number_length;
         $last_number = $increment_last_number;
-        $emp = "";
-        for($i=0; $i<$E_length;$i++)
-        {
-            $emp.="0";
+        $emp = '';
+        for ($i = 0; $i < $E_length; $i++) {
+            $emp .= '0';
         }
 
-        $unique_id = $prefix.$emp.$last_number;
+        $unique_id = $prefix . $emp . $last_number;
 
         $employee = new Employee();
-        // $employee->image=$req->file('image')->store('employee');
-        if($req->hasfile('image')){
+        if ($req->hasfile('image')) {
             $file = $req->file('image');
             $extension = $file->getClientOriginalExtension();
-            $filename = time() .'.'.$extension;
-            $file->move('upload/employeeimg/',$filename);
-            $employee->image = 'upload/employeeimg/'.$filename;
+            $filename = time() . '.' . $extension;
+            $file->move('upload/employeeimg/', $filename);
+            $employee->image = 'upload/employeeimg/' . $filename;
         }
-        $employee->E_Id=$unique_id;
+        $employee->E_Id = $unique_id;
         $employee->userName = $req->input('userName');
         $employee->role = $req->input('role');
         $employee->email = $req->input('email');
@@ -231,27 +236,20 @@ class EmployeeController extends Controller
 
     public function deleteemployee($employeeEditIdData)
     {
-
         Employee::whereId($employeeEditIdData)->delete();
-        // $employee = Employee::find($employeeEditIdData);
-        // $employee->delete;
-
-        return "Record deleted";
+        return 'Record deleted';
     }
 
     public function updateemployee($employeeEditIdData)
     {
-        // $employee = Employee::find($employeeEditIdData);
         Employee::whereId($employeeEditIdData)->update();
-
-        return "Record Updated";
-
+        return 'Record Updated';
     }
 
     public function updatesaveemployee(Request $req, $employeeEditIdData)
     {
         $employee = Employee::find($employeeEditIdData);
-        $employee->image=$req->file('image')->store('employee');
+        $employee->image = $req->file('image')->store('employee');
         $employee->userName = $req->input('userName');
         $employee->role = $req->input('role');
         $employee->email = $req->input('email');
